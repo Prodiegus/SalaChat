af0b8365b89bba280323503c5daa5533fd702f5b
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-10-29 18:13:23 -0300

    Adaptacion db

--- init-mongo.js
+++ init-mongo.js
@@ -1,16 +1,10 @@
 db = db.getSiblingDB('chatpyme');
 
 db.createCollection("usuarios");
-db.createCollection("mensajes");
 
 db.usuarios.createIndex({ nombre: 1 }, { unique: true });
 
 db.usuarios.insertMany([
-  { nombre: "usuario1", clave: "clave1", admin: true },
-  { nombre: "usuario2", clave: "clave2", admin: false }
-]);
-
-db.mensajes.insertMany([
-  { mensajes: ["mensaje1", "mensaje2"], logs: ["log1", "log2"] },
-  { mensajes: ["mensaje3", "mensaje4"], logs: ["log3", "log4"] }
+  { nombre: "usuario1", clave: "clave1", admin: true, mensajes: ["mensaje1", "mensaje2"], grupo: "admin"},
+  { nombre: "usuario2", clave: "clave2", admin: false, mensajes: ["mensaje3", "mensaje4"], grupo: "medico"}
 ]);
